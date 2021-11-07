b30fd01eb8cd0d8f5a02d4e8ed34fab7e4f9edce
huyqd <quanghuy.dang210@gmail.com>
2021-11-07 14:08:16 +0100

    update

--- dataset.py
+++ dataset.py
@@ -1,32 +1,9 @@
 from pathlib import Path
 
-import pytorch_lightning as pl
 import torch
 from torch.utils.data import Dataset, DataLoader
 
 CURRENT_PATH = Path(__file__).cwd()
-
-
-class ML1mDataset(Dataset):
-    def __init__(self):
-        self.train = torch.load(CURRENT_PATH / 'data' / 'ml-1m-train.pt')
-        self.n_users, self.n_items = self.train.size()
-        self.train_pos = self.train._indices().T
-        self.test_pos = torch.load(CURRENT_PATH / 'data' / 'ml-1m-test-pos.pt')
-        self.test_neg = torch.load(CURRENT_PATH / 'data' / 'ml-1m-test-neg.pt')
-
-        test_items = []
-        for u in range(self.n_users):
-            items = torch.cat((self.test_pos[u, 1].view(1), self.test_neg[u]))
-            test_items.append(items)
-
-        self.test_items = torch.vstack(test_items)
-
-    def __len__(self):
-        return self.n_users
-
-    def __getitem__(self, idx):
-        return self.train_pos[idx]
 
 
 class TrainDataset(Dataset):
@@ -63,3 +40,11 @@ class TestDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.test_items[idx], self.test_pos[idx]
+
+
+class ML1mDataset:
+    def __init__(self):
+        self.train_ds = TrainDataset()
+        self.test_ds = TestDataset()
+        self.train_dl = DataLoader(self.train_ds, batch_size=64, shuffle=True)
+        self.test_dl = DataLoader(self.test_ds, batch_size=128, shuffle=False)
