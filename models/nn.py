1c8bf9577f3614d7e0b61fd11bedf58066914bd0
huyqd <quanghuy.dang210@gmail.com>
2021-11-13 13:11:43 +0100

    Update nn, rename mf

--- models/nn.py
+++ models/nn.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pytorch_lightning as pl
 import torch
 from torch import nn
 
@@ -14,15 +12,26 @@ class GMF(nn.Module):
         self.item_embedding = nn.Embedding(
             num_embeddings=n_items, embedding_dim=embedding_dim
         )
-        self.fc = nn.Linear(embedding_dim, 1)
+        self.linear = nn.Linear(embedding_dim, 1)
+
+        self.init_weight()
 
     def forward(self, users, items):
         user_embeddings = self.user_embedding(users)
         item_embeddings = self.item_embedding(items)
         embeddings = user_embeddings.mul(item_embeddings)
-        output = self.fc(embeddings)
+        output = self.linear(embeddings)
 
         return output.squeeze()
+
+    def init_weight(self):
+        nn.init.normal_(self.user_embedding.weight, std=0.01)
+        nn.init.normal_(self.item_embedding.weight, std=0.01)
+        nn.init.xavier_uniform_(self.linear.weight)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                m.bias.data.zero_()
 
 
 class MLP(nn.Module):
@@ -35,33 +44,47 @@ class MLP(nn.Module):
         self.item_embedding = nn.Embedding(
             num_embeddings=n_items, embedding_dim=embedding_dim
         )
-        self.fc1 = nn.Linear(embedding_dim * 2, embedding_dim)
-        self.fc2 = nn.Linear(embedding_dim, int(embedding_dim / 2))
-        self.fc3 = nn.Linear(int(embedding_dim / 2), 1)
 
-        self.dropout = nn.Dropout(p=dropout)
+        linear_dims = (embedding_dim * 2, embedding_dim, int(embedding_dim / 2), 1)
+        self.linear_layers = nn.ModuleList()
+        for idx, (in_dim, out_dim) in enumerate(zip(linear_dims[:-1], linear_dims[1:])):
+            self.linear_layers.append(nn.Linear(in_dim, out_dim))
+            if idx != (len(linear_dims) - 2):  # No activation and dropout for last layers
+                self.linear_layers.append(nn.ReLU())
+                self.linear_layers.append(nn.Dropout(p=dropout))
+
+        self.init_weight()
 
     def forward(self, users, items):
         user_embeddings = self.user_embedding(users)
         item_embeddings = self.item_embedding(items)
-        embeddings = torch.cat([user_embeddings, item_embeddings], axis=1)
-        output = nn.ReLU()(self.fc1(embeddings))
-        output = self.dropout(output)
-        output = nn.ReLU()(self.fc2(output))
-        output = self.dropout(output)
-        output = self.fc3(output)
+        output = torch.cat([user_embeddings, item_embeddings], axis=1)
+        for layer in self.linear_layers:
+            output = layer(output)
 
         return output.squeeze()
 
+    def init_weight(self):
+        nn.init.normal_(self.user_embedding.weight, std=0.01)
+        nn.init.normal_(self.item_embedding.weight, std=0.01)
 
-class NeuMF(pl.LightningModule):
+        for layer in self.linear_layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight)
+
+        for m in self.modules():
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                m.bias.data.zero_()
+
+
+class NeuMF(nn.Module):
     def __init__(self, n_users, n_items, embedding_dim, dropout=0.1):
         super().__init__()
 
-        self.user_embedding = nn.Embedding(
+        self.user_embedding_mlp = nn.Embedding(
             num_embeddings=n_users, embedding_dim=embedding_dim
         )
-        self.item_embedding = nn.Embedding(
+        self.item_embedding_mlp = nn.Embedding(
             num_embeddings=n_items, embedding_dim=embedding_dim
         )
 
@@ -72,182 +95,52 @@ class NeuMF(pl.LightningModule):
             num_embeddings=n_items, embedding_dim=embedding_dim
         )
 
-        self.gmf = nn.Linear(embedding_dim, int(embedding_dim / 2))
+        self.linear_gmf = nn.Linear(embedding_dim, int(embedding_dim / 2))
 
-        self.fc1 = nn.Linear(embedding_dim * 2, embedding_dim)
-        self.fc2 = nn.Linear(embedding_dim, embedding_dim)
-        self.fc3 = nn.Linear(embedding_dim, int(embedding_dim / 2))
+        linear_mlp_dims = (embedding_dim * 2, embedding_dim, embedding_dim, int(embedding_dim / 2))
+        self.linear_mlp_layers = nn.ModuleList()
+        for idx, (in_dim, out_dim) in enumerate(zip(linear_mlp_dims[:-1], linear_mlp_dims[1:])):
+            self.linear_mlp_layers.append(nn.Linear(in_dim, out_dim))
+            if idx != (len(linear_mlp_dims) - 2):  # No activation and dropout for last layers
+                self.linear_mlp_layers.append(nn.ReLU())
+                self.linear_mlp_layers.append(nn.Dropout(p=dropout))
 
-        self.fc_final = nn.Linear(embedding_dim, 1)
+        self.linear_final = nn.Linear(embedding_dim, 1)
 
-        self.dropout = nn.Dropout(p=dropout)
+        self.init_weight()
 
     def forward(self, users, items):
-        user_embeddings = self.user_embedding(users)
-        item_embeddings = self.item_embedding(items)
-        embeddings = torch.cat([user_embeddings, item_embeddings], dim=1)
+        user_embeddings_mlp = self.user_embedding_mlp(users)
+        item_embeddings_mlp = self.item_embedding_mlp(items)
+        embeddings_mlp = torch.cat([user_embeddings_mlp, item_embeddings_mlp], dim=1)
 
         user_embeddings_gmf = self.user_embedding_gmf(users)
         item_embeddings_gmf = self.item_embedding_gmf(items)
         embeddings_gmf = user_embeddings_gmf.mul(item_embeddings_gmf)
 
-        output_gmf = self.gmf(embeddings_gmf)
-        output = nn.ReLU()(self.fc1(embeddings))
-        output = self.dropout(output)
-        output = nn.ReLU()(self.fc2(output))
-        output = self.dropout(output)
-        output = self.fc3(output)
+        output_gmf = self.linear_gmf(embeddings_gmf)
+        output_mlp = embeddings_mlp
+        for layer in self.linear_mlp_layers:
+            output_mlp = layer(output_mlp)
 
-        output = torch.cat([output, output_gmf], dim=1)
-        output = self.fc_final(output)
+        output = torch.cat([output_mlp, output_gmf], dim=1)
+        output = self.linear_final(output)
 
         return output.squeeze()
 
+    def init_weight(self):
+        nn.init.normal_(self.user_embedding_mlp.weight, std=0.01)
+        nn.init.normal_(self.item_embedding_mlp.weight, std=0.01)
+        nn.init.normal_(self.user_embedding_gmf.weight, std=0.01)
+        nn.init.normal_(self.item_embedding_gmf.weight, std=0.01)
 
-class Recommender(pl.LightningModule):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
+        for layer in self.linear_mlp_layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight)
 
-    def forward(self, users, items):
-        return self.model(users, items)
+        nn.init.xavier_uniform_(self.linear_gmf.weight)
+        nn.init.xavier_uniform_(self.linear_final.weight)
 
-    def training_step(self, batch, batch_idx):
-        users, items, labels = batch
-        logits = self(users, items)
-        loss = self.loss_fn(logits, labels)
-
-        return {
-            "loss": loss,
-            "logits": logits.detach(),
-            "batch_length": labels.shape[0],
-        }
-
-    def training_epoch_end(self, outputs):
-        # This function recevies as parameters the output from "training_step()"
-        # Outputs is a list which contains a dictionary like:
-        # [{'pred':x,'target':x,'loss':x}, {'pred':x,'target':x,'loss':x}, ...]
-        pass
-
-    def validation_step(self, batch, batch_idx):
-        users, items, labels = batch
-        logits = self(users, items)
-        loss = self.loss_fn(logits, labels)
-
-        return {
-            "loss": loss.detach(),
-            "logits": logits.detach(),
-            "batch_length": labels.shape[0],
-        }
-
-    def validation_epoch_end(self, outputs):
-        loss = [o["loss"].mul(o["batch_length"]) for o in outputs]
-        length = [o["batch_length"] for o in outputs]
-
-        val_loss = sum(loss) / sum(length)
-        self.log("Val Loss", val_loss, prog_bar=True)
-
-        scores = [o["logits"] for o in outputs]
-        scores = torch.cat(scores).squeeze().numpy()
-
-        scores = scores.reshape(-1, n_items)
-        apak = get_apak(scores, test)
-
-        # Save the metric
-        self.log("Val Apak", apak, prog_bar=True)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters())
-        return optimizer
-
-    def loss_fn(self, logits, labels):
-        return nn.BCEWithLogitsLoss()(logits, labels)
-
-
-class DNSRecommender(pl.LightningModule):
-    def __init__(self, model):
-        super().__init__()
-        self.model = model
-
-    def forward(self, users, items):
-        return self.model(users, items)
-
-    def training_step(self, batch, batch_idx):
-        idx_users, csr_arr, popdist = batch
-
-        nonzero = torch.nonzero(csr_arr, as_tuple=True)
-        popdist[nonzero] = 0
-        popdist = np.divide(popdist, popdist.sum(axis=1, keepdims=True))
-
-        users = []
-        items = []
-        labels = []
-        for id_ in np.arange(idx_users.shape[0]):
-            pos_items = torch.nonzero(csr_arr[id_], as_tuple=True)[0]
-            pos_users = np.repeat(idx_users[id_], pos_items.shape[0])
-            pos_labels = np.zeros(pos_items.shape[0])
-
-            users.append(pos_users)
-            items.append(pos_items)
-            labels.append(pos_labels)
-
-            neg_items = np.random.choice(np.arange(n_items), size=50, p=popdist[id_, :])
-            neg_users = np.repeat(idx_users[id_], neg_items.shape[0])
-            neg_labels = np.zeros(neg_items.shape[0])
-
-            users.append(neg_users)
-            items.append(neg_items)
-            labels.append(neg_labels)
-
-        users = torch.from_numpy(np.concatenate(users))
-        items = torch.from_numpy(np.concatenate(items))
-        labels = torch.from_numpy(np.concatenate(labels))
-
-        logits = self(users, items)
-        loss = self.loss_fn(logits, labels)
-
-        return {
-            "loss": loss,
-            "logits": logits.detach(),
-            "batch_length": labels.shape[0],
-        }
-
-    def training_epoch_end(self, outputs):
-        # This function recevies as parameters the output from "training_step()"
-        # Outputs is a list which contains a dictionary like:
-        # [{'pred':x,'target':x,'loss':x}, {'pred':x,'target':x,'loss':x}, ...]
-        pass
-
-    def validation_step(self, batch, batch_idx):
-        users, items, labels = batch
-        logits = self(users, items)
-        loss = self.loss_fn(logits, labels)
-
-        return {
-            "loss": loss.detach(),
-            "logits": logits.detach(),
-            "batch_length": labels.shape[0],
-        }
-
-    def validation_epoch_end(self, outputs):
-        loss = [o["loss"].mul(o["batch_length"]) for o in outputs]
-        length = [o["batch_length"] for o in outputs]
-
-        val_loss = sum(loss) / sum(length)
-        self.log("Val Loss", val_loss, prog_bar=True)
-
-        scores = [o["logits"] for o in outputs]
-        scores = torch.cat(scores).squeeze().numpy()
-
-        scores = scores.reshape(-1, n_items)
-        apak = get_apak(scores, test)
-
-        # Save the metric
-        self.log("Val Apak", apak, prog_bar=True)
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.AdamW(self.parameters())
-        return optimizer
-
-    def loss_fn(self, logits, labels):
-        return nn.BCEWithLogitsLoss()(logits, labels)
+        for m in self.modules():
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                m.bias.data.zero_()
