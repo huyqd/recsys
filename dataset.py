9ea8c42829364dbdc098878f7d15477b9976d37a
huyqd <quanghuy.dang210@gmail.com>
2022-01-14 21:00:23 -0800

    Add rating data module

--- dataset.py
+++ dataset.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from typing import Optional
 
+import numpy as np
+import pandas as pd
 import pytorch_lightning as pl
 import torch
+from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
 
 CURRENT_PATH = Path(__file__).cwd()
@@ -84,7 +87,7 @@ class BinaryML1mDataModule(pl.LightningDataModule):
 
     def train_dataloader(self):
         users, items, labels = self._negative_sampling()
-        self.train_ds = ML1mDataset(users, items, labels)
+        self.train_ds = BinaryML1mDataset(users, items, labels)
 
         train_dl = DataLoader(self.train_ds, batch_size=self.batch_size, shuffle=True, num_workers=self.n_workers)
         self.train_steps = len(train_dl)
@@ -92,7 +95,7 @@ class BinaryML1mDataModule(pl.LightningDataModule):
         return train_dl
 
     def val_dataloader(self):
-        self.test_ds = ML1mDataset(self.test_users, self.test_items, self.test_labels)
+        self.test_ds = BinaryML1mDataset(self.test_users, self.test_items, self.test_labels)
 
         test_dl = DataLoader(self.test_ds, batch_size=512, shuffle=False, num_workers=self.n_workers)
         self.test_steps = len(test_dl)
@@ -100,7 +103,63 @@ class BinaryML1mDataModule(pl.LightningDataModule):
         return test_dl
 
 
-class ML1mDataset(Dataset):
+class RatingML1mDataModule(pl.LightningDataModule):
+    def __init__(self, batch_size=128, n_workers=8):
+        super().__init__()
+        self.batch_size = batch_size
+        self.n_workers = n_workers
+        self.data_dir = CURRENT_PATH / "data" / "ml-1m"
+
+        self.ratings_sparse = self._get_sparse_ratings()
+        self.n_users, self.n_items = self.ratings_sparse.size()
+
+        self.train_sparse = None
+        self.test_sparse = None
+
+        self.train_ds = None
+        self.test_ds = None
+
+    def prepare_data(self):
+        pass
+
+    def setup(self, stage: Optional[str] = None):
+        if stage == "fit" or stage is None:
+            idx, values = self.ratings_sparse.indices().T, self.ratings_sparse.values()
+            train_idx, test_idx, train_values, test_values = train_test_split(idx, values, test_size=0.1)
+            self.train_sparse = torch.sparse_coo_tensor(train_idx, train_values).coalesce()
+            self.test_sparse = torch.sparse_coo_tensor(test_idx, test_values).coalesce()
+
+        if stage == "test" or stage is None:
+            pass
+
+    def _get_sparse_ratings(self):
+        ratings = pd.read_csv("ratings.dat",
+                              sep="::",
+                              header=None,
+                              names=['user_id', 'movie_id', 'rating', 'timestamp'],
+                              dtype={'user_id': np.int32, 'movie_id': np.int32,
+                                     'ratings': np.float32, 'timestamp': np.int64},
+                              engine='python',
+                              encoding="ISO-8859-1")
+
+        idx = ratings[['user_id', 'movie_id']].sub(1).values.T
+        values = ratings['rating'].values
+        sparse_ratings = torch.sparse_coo_tensor(idx, values).coalesce()
+
+        return sparse_ratings
+
+    def train_dataloader(self):
+        self.train_ds = RatingML1mDataset(self.train_sparse)
+
+        return DataLoader(self.train_ds, batch_size=self.batch_size, shuffle=True, num_workers=self.n_workers)
+
+    def val_dataloader(self):
+        self.test_ds = RatingML1mDataset(self.test_sparse)
+
+        return DataLoader(self.test_ds, batch_size=self.batch_size * 2, shuffle=False, num_workers=self.n_workers)
+
+
+class BinaryML1mDataset(Dataset):
     def __init__(self, users, items, labels=None):
         assert users.shape == items.shape
         if labels is not None:
@@ -117,3 +176,14 @@ class ML1mDataset(Dataset):
             return self.users[idx], self.items[idx], self.labels[idx]
         else:
             return self.users[idx], self.items[idx], None
+
+
+class RatingML1mDataset(Dataset):
+    def __init__(self, sparse):
+        self.sparse = sparse
+
+    def __len__(self):
+        return self.sparse.size()[0]
+
+    def __getitem__(self, idx):
+        return self.sparse[idx]
